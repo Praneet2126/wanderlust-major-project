0ba5e8493fc810c88d5ae55bfd9611257d9e3746
praneetvk <praneetvkhp@gmail.com>
2024-03-18 20:25:24 +0530

    Added styles and extra features

--- app.js
+++ app.js
@@ -84,6 +84,10 @@ app.use((req,res,next) => {
     next();
 });
 
+app.get("/",(req,res) => {
+    res.redirect("/listings");
+})
+
 
 app.get("/demouser", async (req,res) => {
     let fakeUser = new User({
